76cdf7551ddbd473ffdcd02e99a876b6df9a5536
Josh <josh@launchbadge.com>
2018-12-17 16:10:28 -0800

    many new things

--- src/bin.rs
+++ src/bin.rs
@@ -1,12 +1,36 @@
 #![feature(uniform_paths)]
 
 extern crate structopt;
+extern crate colored;
+extern crate openssl;
 
 pub(crate) mod crypto;
 mod proto;
 
 use crypto::SecretKey;
 use structopt::StructOpt;
+use std::io;
+use colored::Colorize;
+use std::io::Read;
+use std::io::Write;
+use openssl::bn::BigNumContext;
+use openssl::ec::*;
+use openssl::nid::Nid;
+use openssl::pkey::PKey;
+use std::fs::File;
+
+macro_rules! prompt {
+   ($($arg:tt),*) => ({
+            let mut i = String::new();
+
+            print!($($arg,)*);
+            std::io::stdout().flush();
+
+            std::io::stdin().read_line(&mut i);
+
+            i.trim().to_string()
+    })
+}
 
 #[derive(StructOpt, Debug)]
 #[structopt(name="hedera keygen", about = "Private and Public key generator for Ħedera")]
@@ -16,49 +40,124 @@ enum HederaKeygen{
     #[structopt(name="generate")]
     Generate {
 
-        /// include a password for mnemonic generation (defaults to "")
-        #[structopt(long = "password", short = "p")]
-        password: Option<String>,
+        /// Print the unencrypted keys and mnemonic phrase  to the terminal
+        #[structopt(long = "unencrypted", short = "u")]
+        unencrypted: bool,
     },
 
     /// Recover a Public/Private key pair by providing the mnemonic
     #[structopt(name="recover")]
     Recover {
-        /// The required mnemonic
-        mnemonic: Vec<String>,
+        /// Print the unencrypted keys and mnemonic phrase  to the terminal
+        #[structopt(long = "unencrypted", short = "u")]
+        unencrypted: bool,
+    },
 
-        /// provide the password used for generation (defaults to "")
-        #[structopt(long = "password", short = "p")]
-        password: Option<String>
+    /// inspect your key
+    #[structopt(name="inspect")]
+    Inspect {
+        /// the pub or pem file you want to inspect
+        file: String,
+    },
+}
+
+
+fn command_generate(unencrypted: bool) {
+
+    println!("Generating public/private {} key pair.", "ed25519".color("red"));
+
+    let out_file = prompt!("Enter file name in which to save the key to: ");
+
+    let passphrase = prompt!("Enter passphrase (empty for no passphrase): ");
+
+    if !passphrase.is_empty() {
+
+        let mut conf_passphrase = prompt!("Enter your passphrase again: ");
+
+        while conf_passphrase != passphrase {
+            conf_passphrase = prompt!("Passphrase did not match, try again: ");
+        }
     }
+
+    let (secret_key, mnemonic) = SecretKey::generate(&passphrase);
+
+    if unencrypted {
+
+        println!("Secret Key: {}", secret_key.to_string().color("green"));
+        println!("Public Key: {}", secret_key.public().to_string().color("yellow"));
+        println!("Mnemonic: {}", mnemonic.color("magenta"));
+
+        return
+    }
+
+    println!("Your public key has been saved in {}{}", out_file.color("blue"), ".pub".color("blue"));
+    println!("Your private key has been saved in {}", out_file.color("blue"));
+
+    println!("You can use this phrase to recover your keys: ");
+    println!("{}", mnemonic.color("magenta"));
 }
 
-fn command_generate(password: Option<String>) {
-    let (secret_key, mnemonic) = SecretKey::generate(&password.unwrap_or_default());
+fn command_recover(unencrypted: bool) {
+    let recovery_phrase = prompt!("Enter your recovery phrase: ");
 
-    println!("Secret Key: {}", secret_key.to_string());
-    println!("Public Key: {}", secret_key.public().to_string());
-    println!("Mnemonic: {}", mnemonic);
-}
+    let out_file = prompt!("Enter file name in which to save the key to: ");
 
-fn command_recover(mnemonic: Vec<String>, password: Option<String>) {
+    let passphrase = prompt!("Enter passphrase (empty for no passphrase): ");
+
+    if !passphrase.is_empty() {
+
+        let mut conf_passphrase = prompt!("Enter your passphrase again: ");
+
+        while conf_passphrase != passphrase {
+            conf_passphrase = prompt!("Passphrase did not match, try again: ");
+        }
+    }
+
     let secret_key = SecretKey::from_mnemonic(
-        &mnemonic.join(" "),
-        &password.unwrap_or_default()
+        &recovery_phrase,
+        &passphrase,
     ).unwrap_or_else(|_| {
-        panic!("Something went wrong, please try again. You can use hedera-keygen --help for more info.");
+        panic!("{}, please try again. You can use hedera-keygen --help for more info.", "Something went wrong".color("red"));
     });
 
-    println!("Secret Key: {}", secret_key.to_string());
-    println!("Public Key: {}", secret_key.public().to_string());
+    if unencrypted {
+        println!("Secret Key: {}", secret_key.to_string().color("green"));
+        println!("Public Key: {}", secret_key.public().to_string().color("yellow"));
+        return
+    }
+
+    println!("Your public key has been saved in {}{}", out_file.color("blue"), ".pub".color("blue"));
+    println!("Your private key has been saved in {}", out_file.color("blue"));
 }
 
+fn command_inspect(file: String) {
+
+    //let mut key_file = File::open(file)?;
+
+    // todo: open the file
+    if file.ends_with(".pub") {
+        unimplemented!()
+
+
+
+    } else if file.ends_with(".pem") {
+        unimplemented!()
+
+
+    }
+
+
+
+
+}
 
 fn main() {
     let command = HederaKeygen::from_args();
 
     match command {
-        HederaKeygen::Generate { password } => command_generate(password),
-        HederaKeygen::Recover { mnemonic, password } => command_recover(mnemonic, password),
+        HederaKeygen::Generate { unencrypted } => command_generate(unencrypted),
+        HederaKeygen::Recover { unencrypted } => command_recover(unencrypted),
+        HederaKeygen::Inspect { file } => command_inspect(file),
     }
 }
+
